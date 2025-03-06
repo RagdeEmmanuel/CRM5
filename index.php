ab3aa827c98978aad8749fe82239198391bb921b
RagdeEmmanuel <edgaremmanuelaguirrevilledo@gmail.com>
2025-03-06 15:51:09 -0600

    Cambios para descargar

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "<h1>Hola, $nombre!</h1>";
 } else {
     // Si no se ha enviado el formulario, muestra el formulario
-    echo '<h1>Bienvenido a la página PHP</h1>';
+    echo '<h1>Bienvenido a la página PHP usuario</h1>';
     echo '<form method="POST" action="">
             <label for="nombre">Ingresa tu nombre:</label>
             <input type="text" id="nombre" name="nombre" required>
